7743b9c99b0d1342da874f30f4e08ad0dd39befa
andrewwachira <andrewwachira1@gmail.com>
2023-12-19 21:37:21 +0300

    enabled ssl for pg connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import {pool} from "../../utils/db";
 
 export default async  function handler(req, res) {
@@ -7,7 +6,8 @@ export default async  function handler(req, res) {
     const client = await pool.connect();
     await client.query('CREATE TABLE IF NOT EXISTS Users(name VARCHAR(255) NOT NULL,email VARCHAR(255) UNIQUE NOT NULL,phone VARCHAR(255),password VARCHAR(255),id  SERIAL NOT NULL,user_photo VARCHAR(255),isadmin BOOLEAN DEFAULT false,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,google_signin_id VARCHAR(255),isverified BOOLEAN DEFAULT false,PRIMARY KEY (id))')
     await client.query('CREATE TABLE IF NOT EXISTS Products (admin INTEGER NOT NULL,name VARCHAR(255) NOT NULL,brand VARCHAR(255) NOT NULL,category VARCHAR(255) NOT NULL,price INTEGER NOT NULL,description VARCHAR(255),image VARCHAR(255) NOT NULL,count_in_stock INTEGER NOT NULL,rating INTEGER NOT NULL DEFAULT 0,reviews JSONB[],product_id  SERIAL NOT NULL,discount INTEGER,condition VARCHAR(255) NOT NULL,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (product_id),FOREIGN KEY (admin) REFERENCES Users(id))');    
-    const newUser = await client.query('INSERT INTO users (name, email, phone, password, isadmin, isverified) VALUES ($1, $2, $3) RETURNING *',["Admin User", "decode3.it@gmail.com", "254711294124","$2b$10$YTAkTsMGst0mUwuJemis4u1/Sa/kDb3SYYClQPMvhz0d.cx5idCJ.",true,true]);
+    const newUser = await client.query('INSERT INTO users (name, email, phone, password, isadmin, isverified) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',["Admin User", "decode3.it@gmail.com", "254711294124","$2b$10$YTAkTsMGst0mUwuJemis4u1/Sa/kDb3SYYClQPMvhz0d.cx5idCJ.",true,true]);
+   
     res.json({adminUser: newUser.rows[0],database:process.env.POSTGRES_DATABASE});
   } catch (error) {
     res.json({error:error,database:process.env.POSTGRES_DATABASE});
--- utils/db.js
+++ utils/db.js
@@ -6,5 +6,6 @@ export const pool = new Pool({
     database: process.env.NODE_ENV === 'development' ? 'decode-tech' : process.env.POSTGRES_DATABASE,
     password: process.env.NODE_ENV === 'development' ? "8080" : process.env.POSTGRES_PASSWORD,
     port: 5432, // default PostgreSQL port
+    ssl:true
   });
 
